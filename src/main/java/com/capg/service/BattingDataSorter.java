928152b49979507d80843a6c59eae2f607db05dc
jindalakhil <akhiljindal222@gmail.com>
2020-10-31 00:06:06 +0530

    [Akhil]Added: Comparing batsman acoording to max strike rate and maximum number of sixes and fours

--- src/main/java/com/capg/service/BattingDataSorter.java
+++ src/main/java/com/capg/service/BattingDataSorter.java
@@ -10,4 +10,6 @@ public class BattingDataSorter {
 	public static Comparator<IPLBatting> TOP_STRIKING_RATE = Comparator.comparing(IPLBatting::getStrikeRate);
 	
 	public static Comparator<IPLBatting> MAX_SIXES_AND_FOURS = Comparator.comparing(batsman -> batsman.getNoOfSixes() + batsman.getNoOfFours());
+
+	 public static Comparator<IPLBatting> BEST_SR_WITH_MAX_SIXES_AND_FOURS = MAX_SIXES_AND_FOURS.thenComparing(IPLBatting::getStrikeRate);
 }
